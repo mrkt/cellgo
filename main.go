253d619824ab2f33e053999282c087be0814987b
tommy jin <ifen806@sina.com>
2016-07-10 23:29:16 +0800

    Add main file by go

---
+++ main.go
@@ -0,0 +1,18 @@
+// http.service project main.go
+package main
+
+import (
+	"net/http"
+)
+
+func SayHello(w http.ResponseWriter, req *http.Request) {
+
+	w.Write([]byte("hello!"))
+}
+
+func main() {
+
+	http.HandleFunc("/hello", SayHello)
+	http.ListenAndServe(":8001", nil)
+
+}
