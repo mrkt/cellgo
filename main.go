2089d9302627d69a52f3bad53c6b22bfa51fa8f7
tommy jin <ifen806@sina.com>
2016-07-12 03:10:04 +0800

    Prepare a simple sample

--- main.go
+++ main.go
@@ -1,19 +1,29 @@
-// http.service project main.go
 package main
 
 import (
-	"net/http"
+    "fmt"
+    "net/http"
+    "strings"
+    "log"
 )
 
-func SayHello(w http.ResponseWriter, req *http.Request) {
-
-	w.Write([]byte("hello!"))
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()  //解析参数，默认是不会解析的
+    fmt.Println(r.Form)  //这些信息是输出到服务器端的打印信息
+    fmt.Println("path", r.URL.Path)
+    fmt.Println("scheme", r.URL.Scheme)
+    fmt.Println(r.Form["url_long"])
+    for k, v := range r.Form {
+        fmt.Println("key:", k)
+        fmt.Println("val:", strings.Join(v, ""))
+    }
+    fmt.Fprintf(w, "Hello astaxie!") //这个写入到w的是输出到客户端的
 }
 
 func main() {
-
-	http.HandleFunc("/hello", SayHello)
-	http.ListenAndServe(":8001", nil)
-
+    http.HandleFunc("/", sayhelloName) //设置访问的路由
+    err := http.ListenAndServe(":9090", nil) //设置监听的端口
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
 }
-
