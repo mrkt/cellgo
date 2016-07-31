61e1dd314fb8d43b7146afec9415e16c70c16c57
tommy jin <ifen806@sina.com>
2016-07-31 23:25:18 +0800

    Add new test cases

--- example/controllers/userController.go
+++ example/controllers/userController.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"fmt"
 	"library/service"
 
 	"github.com/mrkt/cellgo"
@@ -28,16 +27,39 @@ func (this *UserController) Run() {
 func (this *UserController) Add() {
 	username := this.Net.Input.GetGP("username", true)
 	email := this.Net.Input.GetGP("email", true)
-	if user := this.Net.Input.Session.Get("user"); user != nil {
-		fmt.Println(user.(map[string]string))
+	this.Data["Username"] = username
+	this.Data["Email"] = email
+	this.Data["URI"] = this.Net.Input.Site() + this.Net.Input.URI()
+	this.TplName = "index.html"
+}
+
+func (this *UserController) Session() {
+	if res := this.Net.Input.Session.Get("user"); res != nil {
+		user := res.(map[string]string)
+		this.Data["Username"] = user["username"]
+		this.Data["Email"] = user["email"]
 	} else {
 		var user map[string]string = make(map[string]string)
 		user["username"] = "tommy"
 		user["email"] = "tommy.jin@aliyun.com"
 		this.Net.Input.Session.Set("user", user)
 	}
-	this.Data["Username"] = username
-	this.Data["Email"] = email
+	this.Data["URI"] = this.Net.Input.Site() + this.Net.Input.URI()
+	this.TplName = "index.html"
+}
+
+func (this *UserController) Cookie() {
+	if res := this.Net.Input.Cookie.Get("user"); res != nil {
+		user := res.(map[string]string)
+		this.Data["Username"] = user["username"]
+		this.Data["Email"] = user["email"]
+	} else {
+		var user map[string]string = make(map[string]string)
+		user["username"] = "tommy"
+		user["email"] = "tommy.jin@aliyun.com"
+		this.Net.Input.Cookie.Set("user", user)
+
+	}
 	this.Data["URI"] = this.Net.Input.Site() + this.Net.Input.URI()
 	this.TplName = "index.html"
 }
